177aff3ca7bc38c976dc1a9190d85002490b5462
Olugbenga Alao <gasttronomy@gmail.com>
2021-01-21 03:50:21 +0100

    A wimpy chapter 7 commit

---
+++ src/Self_Exercises.java
@@ -0,0 +1,35 @@
+public class Self_Exercises {
+   /* 7.1
+    a. Rows and Columns(Wrong); (Arrays & Collections)
+    b. data , types (Wrong), (variables & type)
+    c. Enhanced for loop
+    d. Index.
+    e. Multidimensional array (wrong), (Tw0-dimensional)
+    f. for (double d : numbers).
+    g.  Strings array
+    h.  args[0] ; args.length
+    i.  test
+    j. ellipsis
+
+    7.2
+    a. False
+    b. False
+    c. True
+    d. False
+
+    7.3
+    a. public final int ARRAY_SIZE = 10;
+    b. double [] Average = new double [ARRAY_SIZE]
+    c. Average [4]
+    d. Average[9] = 1.667
+    e. Average[6] = 6
+    f.
+    int total = 0;
+    for(i = 0; i < Average.length; i++){
+        total+=Average[i];
+        sout(total);
+    }
+    */
+
+
+}
